70f7c1edf47c107c2be3c8f3efc46dabee65ff2e
Xari Mouchtouris <xarimouchtouris@Xaris-MacBook-Air.local>
2025-01-25 20:06:23 +0200

    Add server.js file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { body, validationResult } = require('express-validator');
 const sanitizeHtml = require('sanitize-html');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware
 
